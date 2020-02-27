347016a137c93dcb95213f77c27956f6cf580e4a
frauzufall <mail@frauzufall.de>
2020-02-27 15:35:19 +0100

    Move ZipDirectory method into N2VUtils

--- src/main/java/de/csbdresden/n2v/N2VUtils.java
+++ src/main/java/de/csbdresden/n2v/N2VUtils.java
@@ -17,6 +17,7 @@ import net.imglib2.view.Views;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -51,10 +52,46 @@ public class N2VUtils {
 		Path out = Files.createTempFile("n2v-trained-model", ".zip");
 		FileOutputStream fos = new FileOutputStream(out.toFile());
 		ZipOutputStream zipOut = new ZipOutputStream(fos);
-		ZipDirectory.zipFile(checkpointDir, null, zipOut);
+		zipFile(checkpointDir, null, zipOut);
 		zipOut.close();
 		fos.close();
 		return out.toFile();
+	}
+
+	static void zipFile(File fileToZip, String fileName, ZipOutputStream zipOut) throws IOException {
+		if (fileToZip.isHidden()) {
+			return;
+		}
+		if (fileToZip.isDirectory()) {
+			if(fileName == null) {
+				File[] children = fileToZip.listFiles();
+				for (File childFile : children) {
+					zipFile(childFile, childFile.getName(), zipOut);
+				}
+			} else {
+				if (fileName.endsWith("/")) {
+					zipOut.putNextEntry(new ZipEntry(fileName));
+					zipOut.closeEntry();
+				} else {
+					zipOut.putNextEntry(new ZipEntry(fileName + "/"));
+					zipOut.closeEntry();
+				}
+				File[] children = fileToZip.listFiles();
+				for (File childFile : children) {
+					zipFile(childFile, fileName + "/" + childFile.getName(), zipOut);
+				}
+			}
+			return;
+		}
+		FileInputStream fis = new FileInputStream(fileToZip);
+		ZipEntry zipEntry = new ZipEntry(fileName);
+		zipOut.putNextEntry(zipEntry);
+		byte[] bytes = new byte[1024];
+		int length;
+		while ((length = fis.read(bytes)) >= 0) {
+			zipOut.write(bytes, 0, length);
+		}
+		fis.close();
 	}
 
 	public static void unZipAll(File source, File destination) throws IOException
