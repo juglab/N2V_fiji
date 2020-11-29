b98dcb0ad6cefcdf2e27dfb572c565b12656497a
frauzufall <mail@frauzufall.de>
2020-11-29 04:05:38 +0100

    UpgradeN2VModelCommand: specify to which specification version this is currently able to upgrade

--- src/main/java/de/csbdresden/n2v/command/UpgradeN2VModelCommand.java
+++ src/main/java/de/csbdresden/n2v/command/UpgradeN2VModelCommand.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -118,6 +118,7 @@ public class UpgradeN2VModelCommand implements Command {
 		Files.copy(new File(model.getLocation().getURI()).toPath(), destination.toPath(), StandardCopyOption.REPLACE_EXISTING);
 		N2VModelSpecification newSpec = new N2VModelSpecification();
 		newSpec.read(oldSpec);
+		newSpec.setFormatVersion("0.2.0-csbdeep");
 		try (FileSystem fileSystem = FileSystems.newFileSystem(destination.toPath(), null)) {
 			Path specPath = fileSystem.getPath(SpecificationWriter.getModelFileName());
 			if(Files.exists(specPath)) Files.delete(specPath);
